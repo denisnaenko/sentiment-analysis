3eb98efd77ffed0bc00d2c6ccc0c9a9b51fbc40c
grinnbis <ngrinnbi@gmail.com>
2024-06-07 08:53:30 +0500

    Add comment_analysis_model.joblib to Git LFS

--- analyser/comment_model_setup.py
+++ analyser/comment_model_setup.py
@@ -12,13 +12,12 @@ random_state = 42
 
 punctuation_marks = ('!', ',', '(', ')', ':', '-', '+', '?', '.', '«', '»', ';', '–', '-', '…', '—',
                       '#', '&', '%', '*', '“', '”', '`', "'", '"', '0', '1', '2', '3', '4', '5', '6', '7', '8',
-                      '9', 's', 'x', 'l', '\\', '/') + tuple(ascii_lowercase)
+                      '9', '\\', '/') + tuple(ascii_lowercase)
 
-stop_words = stopwords.words("russian") + ['это', 'аж', 'ка', 'мя', 'ул', 'ом', 'ма', 'ый', 'мр', 'тр', 'чей',
-                                           'сгс', 'ля', 'эппл', 'юсб', 'тлф', 'ася', 'азс', 'ик', 'нуть', 'юрий',
-                                           'тат', 'эл', 'ух', 'джи', 'дог', 'грн', 'ия', 'изз', 'самс', 'дак', 'пвз',
-                                           'ке', 'ин', 'гпс', 'сап', 'аля', 'хны', 'эх', 'зы']
+# С помощью лемматизации, стематизации, pos-тегов нужно убрать из словаря и обработки этот снизу мусор
+stop_words = stopwords.words("russian") + ['это', 'нибудь']
 stop_words.remove('не')
+
 morph = MorphAnalyzer()
 
 # загрузка модели
@@ -27,25 +26,25 @@ with open('analyser/models/comment_analysis_model.joblib', 'rb') as file:
 
 def remove_emoji(text):
     emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # эмодзи эмоций
-                               u"\U0001F300-\U0001F5FF"  # символы и пиктограммы
-                               u"\U0001F680-\U0001F6FF"  # эмодзи транспорта
-                               u"\U0001F1E0-\U0001F1FF"  # эмодзи флагов стран
-                               u"\U00002500-\U00002BEF"  # китайские иероглифы
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # variation selector
-                               u"\u3030"
-                               "]+", flags=re.UNICODE)
+                           u"\U0001F600-\U0001F64F"  # эмодзи эмоций
+                           u"\U0001F300-\U0001F5FF"  # символы и пиктограммы
+                           u"\U0001F680-\U0001F6FF"  # эмодзи транспорта
+                           u"\U0001F1E0-\U0001F1FF"  # эмодзи флагов стран
+                           u"\U00002500-\U00002BEF"  # китайские иероглифы
+                           u"\U00002702-\U000027B0"
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001f926-\U0001f937"
+                           u"\U00010000-\U0010ffff"
+                           u"\u2640-\u2642"
+                           u"\u2600-\u2B55"
+                           u"\u200d"
+                           u"\u23cf"
+                           u"\u23e9"
+                           u"\u231a"
+                           u"\ufe0f"  # variation selector
+                           u"\u3030"
+                           "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
 
 def preprocess(text, stop_words, punctuation_marks, morph):
@@ -84,8 +83,8 @@ with open('analyser/data_dictionaries/comments_dictionary.json', 'r') as file:
 def text_to_sequence(txt, word_to_index):
     seq = []
     for word in txt:
-        index = word_to_index.get(word, 1)  # 1 означает неизвестное слово
-        # неизвестные слова не добавляем в выходную последовательность
+        index = word_to_index.get(word, 1) # 1 означает неизвестное слово
+        # Неизвестные слова не добавляем в выходную последовательность
         if index != 1:
             seq.append(index)
     return seq
@@ -114,7 +113,7 @@ def vectorize_sequences(sequences, dimension=10000):
     results = zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
         for index in sequence:
-            results[i, index] += 1
+            results[i, index] += 1.
     return results
 
 def analyse_sentiment(comment):
